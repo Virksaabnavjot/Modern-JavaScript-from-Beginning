bafd5e8e6df0882af36b1ada9d2791a884175673
Navjot Singh Virk <virksaabnavjot@gmail.com>
2019-08-10 22:06:57 +0100

    Learning Console Commands

---
+++ app.js
@@ -0,0 +1,7 @@
+console.time('timekeeper');
+console.log("Modern Javascript");
+let siblingsObject = {brother: "Navjot Singh", sister: "Jashan Deep"};
+console.table(siblingsObject);
+console.error("Test Error");
+console.warn("Test Warning");
+console.timeEnd('timekeeper');
